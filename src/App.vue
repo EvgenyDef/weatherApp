31f327e28ee7e639c32f46a702028e5e1fcddbde
Ostapchuk Daniil <ostapman228@gmail.com>
2025-05-01 22:01:55 +0400

    Апишка

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,32 @@
 <script setup>
+import { ref, onMounted } from 'vue';
+import { fetchCurrentWeather } from './api/weather';
+
+const weatherData = ref(null);
+
+onMounted(async () => {
+  try {
+    weatherData.value = await fetchCurrentWeather(); 
+  } catch (error) {
+    console.error("Ошибка загрузки данных:", error);
+  } 
+});
+
 </script>
 
 <template>
-
+<div class="container">
+<div class="block">Куку</div>
+<div class="block"><p>{{ weatherData }}</p></div>
+</div>
 </template>
 
 <style scoped>
-
+.block {
+  display: inline-block;
+  border: 1px solid white;
+  border-radius: 10px;
+  padding: 10px;
+  margin: 10px;
+}
 </style>
---
+++ src/api/weather.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+export const fetchCurrentWeather = async () => {
+  try {
+    const response = await axios.get(
+      'http://api.weatherapi.com/v1/current.json?key=574c020b4a514d5eb0290100252704&q=Samara&aqi=no'
+    );
+
+
+    return response.data.current
+    
+  } catch (error) {
+    console.error('Ошибка при запросе погоды:', error);
+    throw error; 
+  }
+};
